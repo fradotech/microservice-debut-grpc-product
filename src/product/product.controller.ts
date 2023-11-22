5e2950a2debad85beaf55e512b9ee41eb920d176
fradotech <fradotech.id@gmail.com>
2023-11-22 13:35:13 +0700

    feat: expose product list

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { GrpcMethod, Payload } from '@nestjs/microservices';
 import { ProductService } from './product.service';
 
@@ -6,6 +6,7 @@ import { ProductService } from './product.service';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @Get()
   @GrpcMethod('ProductService')
   async findAll() {
     return { data: await this.productService.findAll() };
